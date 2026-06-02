592e9cb3220468c5989f856fb4ca357c57cd24db
Soumyadeep Pal <sanupal328@gmail.com>
2026-06-03 02:00:34 +0530

    renamed user.model.js to user.models.js

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,4 @@
-import { User } from "../models/user.model.js";
+import { User } from "../models/user.models.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
