e755a58815b55aa23267a84c9afb3ffcc8b571a3
Soumyadeep Pal <sanupal328@gmail.com>
2026-06-07 02:56:45 +0530

    updated project.controllers.js

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -7,15 +7,70 @@ import { emailVerificationMailgenContent, forgotPasswordMailgenContent, sendEmai
 import { asyncHandler } from "../utils/async-handler.js";
 import jwt from "jsonwebtoken"
 import crypto from "crypto"
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 import { UserRolesEnum } from "../utils/constants.js";
 
 const getProjects=asyncHandler(async (req,res)=>{
-
+    const projects=await ProjectMember.aggregate([
+        {
+            $match:{
+                user: new mongoose.Types.ObjectId(req.user._id),
+            },
+        },
+        {
+            $lookup:{
+                from:"projects",
+                localField:"projects",
+                foreignField:"_id",
+                as:"projects",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"projectmembers",
+                            localField:"_id",
+                            foreignField:"projects",
+                            as:"projectmembers"
+                        }
+                    },
+                    {
+                        $addFields:{
+                            members:{
+                                $size:"$projectmembers"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind:"$projects"
+        },
+        {
+            $project:{
+                projects:{
+                    _id:1,
+                    name:1,
+                    description:1,
+                    members:1,
+                    createdAt:1,
+                    createdBy:1
+                },
+                role:1,
+                _id:0,
+            }
+        }
+    ])
+    return res.status(200).json(new ApiResponse(200,projects,"Projects fetched successfully"));
 })
 
 const getProjectById=asyncHandler(async (req,res)=>{
-    
+    const {projectId}=req.params
+    const project=await Project.findById(projectId)
+
+    if(!project){
+        throw new ApiError(404,"Project not found")
+    }
+    return res.status(200).json( new ApiResponse(200,project,"Project fetched successfully"));
 })
 
 const createProject=asyncHandler(async (req,res)=>{
@@ -80,7 +135,30 @@ const deleteProject=asyncHandler(async (req,res)=>{
 })
 
 const addMembersToProject=asyncHandler(async (req,res)=>{
-    
+    const {email,role}=req.body
+    const {projectId}=req.params
+    const user= await User.findOne({email})
+
+    if(!user){
+        throw new ApiError(404,"User does not exist")
+    }
+    await ProjectMember.findByIdAndUpdate(
+        {
+            user:new mongoose.Types.ObjectId(user._id),
+            project: new mongoose.Types.ObjectId(projectId)
+        },
+        {
+            user:new mongoose.Types.ObjectId(user._id),
+            project: new mongoose.Types.ObjectId(projectId),
+            role:role
+        },
+        {
+            new:true,
+            upsert:true
+        }
+    )
+
+    return res.status(200).json( new ApiResponse(200,{},"Project Member added successfully"));
 })
 
 const getProjectMembers=asyncHandler(async (req,res)=>{
